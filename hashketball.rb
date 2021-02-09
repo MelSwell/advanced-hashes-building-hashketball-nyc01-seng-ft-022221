30343d77e48126c2382eeaef126a280f3925138f
Seth Cohen <smcohen82@gmail.com>
2021-02-09 19:48:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,13 +2,13 @@
 def game_hash
   {
     :home => {
-      :team_name => nil, 
-      :colors => [], 
+      :team_name => "Brooklyn Nets", 
+      :colors => ["Black", "White"], 
       :players => []
     }, 
     :away => {
-      :team_name => nil, 
-      :colors => [], 
+      :team_name => "Charlotte Hornets", 
+      :colors => ["Turquoise", "Purple"], 
       :players => []
     }
   }
