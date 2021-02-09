1f37306b3e0f68bc0fc5bff5d343fea283114a0d
Seth Cohen <smcohen82@gmail.com>
2021-02-09 19:47:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,15 @@
 # Write your code here!
 def game_hash
-  
+  {
+    :home => {
+      :team_name => nil, 
+      :colors => [], 
+      :players => []
+    }, 
+    :away => {
+      :team_name => nil, 
+      :colors => [], 
+      :players => []
+    }
+  }
 end
